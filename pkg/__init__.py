bb52987146da4faf534a9a290cb426f1a7928bdd
R Lennon <ruth.lennon@lyit.ie>
2021-11-29 21:19:25 +0000

    commit2

---
+++ pkg/__init__.py
@@ -0,0 +1,22 @@
+"""
+# 
+# File        : __init__.py.py
+# Created     ：29/11/2021 21:13
+# Author      ：R.G. Lennon
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Ruth G. Lennon, LYIT
+# 	            Available under GNU Public License (GPL)
+# Description ：
+# 
+"""
+
+if __name__ == '__main__':
+    '''
+      Main method of application 
+
+      Parameters:
+        none
+
+      Returns:
+        none
+    '''
---
+++ pkg/something.py
@@ -0,0 +1,23 @@
+"""
+# 
+# File        : something.py
+# Created     ：29/11/2021 21:13
+# Author      ：R.G. Lennon
+# Version     ：v1.0.0
+# Licencing   : (C) 2021 Ruth G. Lennon, LYIT
+# 	            Available under GNU Public License (GPL)
+# Description ：
+# 
+"""
+
+if __name__ == '__main__':
+    '''
+      Main method of application 
+
+      Parameters:
+        none
+
+      Returns:
+        none
+    '''
+    print("hi!")
